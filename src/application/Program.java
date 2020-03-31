c93a3932a21a2fa9b38deb7427678bfb6f50afae
ronaldo <rondweb@gmail.com>
2020-03-30 21:07:09 -0400

    Commit com orientacao a objeto parte 1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,46 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle x,y;
+		
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("Enter the measures of the triangle x: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		System.out.println("Enter the measures of the triangle y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double p = (x.a + x.b + x.c) / 2.00;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		
+		p = (y.a + y.b + y.c) / 2.00;
+		double areay = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle Y area: %.4f%n", areay);
+		
+		if (areaX > areay) {
+			System.out.println("X is the largest area");
+		}else
+		{
+			System.out.println("Y is the largest area");
+		}
 
 	}
 
